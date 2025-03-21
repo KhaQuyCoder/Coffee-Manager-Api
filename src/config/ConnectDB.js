0b6d8e0cf5e001a28e365b541cfaf4ed34882cdd
KhaQuy <khaquydev04@gmail.com>
2025-03-21 23:36:03 +0700

    fix err connectDB

--- src/config/ConnectDB.js
+++ src/config/ConnectDB.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
     await mongoose.connect("mongodb://127.0.0.1:27017/Cafe_Store");
     console.log("ConnectDB thành công");
   } catch (error) {
-    console.log("Kết nối thất bại" + err);
+    console.log("Kết nối thất bại" + error);
   }
 };
 
