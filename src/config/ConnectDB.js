e6d66f556062e56657438928271d820b2824208f
KhaQuy <khaquydev04@gmail.com>
2025-04-03 15:31:40 +0700

    fix db

--- src/config/ConnectDB.js
+++ src/config/ConnectDB.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb://localhost:27017/Cafe_Store"
-      // "mongodb+srv://khaquydev04:HOKhaQuy2209@coffeestore.jnoaw.mongodb.net/Cafe_Store?retryWrites=true&w=majority&appName=Cafe_Store"
+      // "mongodb://localhost:27017/Cafe_Store"
+      "mongodb+srv://khaquydev04:HOKhaQuy2209@coffeestore.jnoaw.mongodb.net/Cafe_Store?retryWrites=true&w=majority&appName=Cafe_Store"
     );
     console.log("ConnectDB thành công");
   } catch (error) {
